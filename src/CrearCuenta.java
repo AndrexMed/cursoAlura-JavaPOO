e7b94d34c90f38c242aeb445507d490b278c03e6
AndrexMed <giovanniandrex@gmail.com>
2023-03-09 19:01:44 -0500

    Instancias p1

---
+++ src/CrearCuenta.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author giova
+ */
+public class CrearCuenta {
+    public static void main(String[] args) {
+        Cuenta primeraCuenta = new Cuenta();
+        primeraCuenta.saldo = 1000;
+        
+        System.out.println(primeraCuenta.saldo);
+    }
+}
