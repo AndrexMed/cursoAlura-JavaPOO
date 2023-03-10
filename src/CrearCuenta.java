158baefebe991873359fe2e1e0b27ba9f8038a11
AndrexMed <giovanniandrex@gmail.com>
2023-03-09 20:03:29 -0500

    Referencias p2

--- src/CrearCuenta.java
+++ src/CrearCuenta.java
@@ -5,15 +5,21 @@
 public class CrearCuenta {
     public static void main(String[] args) {
         Cuenta primeraCuenta = new Cuenta();
-        primeraCuenta.saldo = 1000;
+        primeraCuenta.saldo = 400;
         
         System.out.println(primeraCuenta.saldo);
         
          Cuenta segundaCuenta = new Cuenta();
-         segundaCuenta.saldo = 500;
+         segundaCuenta.saldo = 400;
          
          System.out.println(segundaCuenta.saldo);
          System.out.println(primeraCuenta.saldo);
+         
+         if (primeraCuenta == segundaCuenta) {
+            System.out.println("Son el mismo objeto");
+        }else{
+            System.out.println("Son diferentes");
+        }
          
     }
 }
