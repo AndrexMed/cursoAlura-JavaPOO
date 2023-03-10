43a1f86b73748031be03feb159820e831b34bb01
AndrexMed <giovanniandrex@gmail.com>
2023-03-09 21:33:25 -0500

    Referenciando objetos

--- src/PruebaMetodos.java
+++ src/PruebaMetodos.java
@@ -13,6 +13,21 @@ public class PruebaMetodos {
         
         miCuenta.retirar(100);
         System.out.println(miCuenta.saldo);
+        
+        Cuenta cuentaAndres = new Cuenta();
+        cuentaAndres.depositar(1000);
+        boolean puedeTransferir = cuentaAndres.transferir(400, miCuenta);
+        
+        if (puedeTransferir) {
+            System.out.println("***** TRANSACCION EXITOSA *****");
+        }
+        else{
+            System.out.println("***** NO FUE POSIBLE TRANSFERIR *****");
+        }
+        
+               
+        System.out.println(cuentaAndres.saldo);
+        System.out.println(miCuenta.saldo);
     }
     
     
